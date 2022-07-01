e92be7dfb2889603e7c0621591fc704ae064067c
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-01 18:59:09 -0500

    style files, and modules moved

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from './components/App/Header';
+import './scss/globals.scss';
+
+function App() {
+  return (
+    <>
+    <main className="mainClass">
+            <Header/>
+    </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/App/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './header.scss';
+
+function Header() {
+    return(
+        <h1>Hello world</h1>
+    );
+};
+
+
+export { Header };
