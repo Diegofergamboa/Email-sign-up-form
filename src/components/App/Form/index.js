74ce6127d6f34f0bce98a17f100f3427b9f8f008
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-03 11:50:25 -0500

    summary

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -4,11 +4,11 @@ import './form.scss';
 function Form() {
     return(
         <form>
-            <label for='inputId'>
-                EMAIL ADDRESS
+            <label for='inputId' className="h2--purple">
+                Email Adress
                 <input type='text' id='inputId' name='inputId'></input>
-            </label>v
-            <button onSubmit={() => alert('Se ha submitido')}>
+            </label>
+            <button onClick={() => alert('Se ha submitido')}>
                 <span>Flechita</span>
             </button>
         </form>
