440279effdf03eea9ff2bfabfabc66b264fbd973
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-03 12:49:00 -0500

    styling on css

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -4,13 +4,9 @@ import './form.scss';
 function Form() {
     return(
         <form>
-            <label for='inputId' className="label-title">
-                Email Adress
-                <input type='text' id='inputId' name='inputId'></input>
-            </label>
-            <button onClick={() => alert('Se ha submitido')}>
-                <span>Flechita</span>
-            </button>
+            <label for='inputId' className="label-title">Email Adress</label>
+            <input type='text' id='inputId' name='inputId'></input>
+            <button onClick={() => alert('Se ha submitido')}></button>
         </form>
     );
 };
