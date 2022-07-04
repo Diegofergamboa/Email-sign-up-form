b0f10de85996b9da7e7e099fcd2afec0cb9ee943
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-03 19:23:37 -0500

    obtainig the values of the form

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import './form.scss';
 
 function Form() {
+
+    const [value, setValue] = React.useState('Put your mail here');
+    
+
+    function handleInput(event) {
+        const inputValue = event.target.value;
+        (!inputValue.includes('@') && !inputValue.includes('.')) ? setValue('Invalid mail') : setValue(inputValue);
+    };
+
+    function handleChangeInput(event){
+        const inputValue = event.target.value;
+        setValue(inputValue);
+    }
     return(
         <form>
-            <label for='inputId' className="label-title">Email Adress</label>
+            <label htmlFor='inputId' className="label-title">Email Adress</label>
             <div className='form--input-button'>
-            <input type='text' id='inputId' class='input-class'name='inputId'></input>
-            <button onClick={() => alert('Se ha submitido')}></button>
+            <input onChange={handleChangeInput} type='text' id='inputId' value={value} placeholder={'Put your mail here'}className='input-class'name='inputId'></input>
+            <button type='button' onClick={handleInput}></button>
             </div>
         </form>
     );
