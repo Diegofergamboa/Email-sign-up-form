5000d9ed346ff506ce072123236d7b7c3a29df74
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-05 17:35:05 -0500

    Update index.js

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -6,9 +6,15 @@ function Form() {
     const [value, setValue] = React.useState('Put your mail here');
     
 
-    function handleInput(event) {
+    function handleButtonInput(event) {
         const inputValue = event.target.value;
-        (!inputValue.includes('@') && !inputValue.includes('.')) ? setValue('Invalid mail') : setValue(inputValue);
+        if (!inputValue.includes('@') && !inputValue.includes('.')){
+            setValue('Invalid mail')
+            debugger
+        } else {
+            setValue('Mail correct')
+            debugger
+        }
     };
 
     function handleChangeInput(event){
@@ -20,7 +26,7 @@ function Form() {
             <label htmlFor='inputId' className="label-title">Email Adress</label>
             <div className='form--input-button'>
             <input onChange={handleChangeInput} type='text' id='inputId' value={value} placeholder={'Put your mail here'}className='input-class'name='inputId'></input>
-            <button type='button' onClick={handleInput}></button>
+            <button type='button' onClick={handleButtonInput}></button>
             </div>
         </form>
     );
