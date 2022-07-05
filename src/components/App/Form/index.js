2fac83ec25dd1197442aa2d615b386d4fe910aad
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-05 18:25:30 -0500

    The handle of the changes, are working, now i can detect if a value is incorrect or correct as an email

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -5,22 +5,22 @@ function Form() {
 
     const [value, setValue] = React.useState('Put your mail here');
     
-
-    function handleButtonInput(event) {
-        const inputValue = event.target.value;
+    // Check if the mail is in the correct format with an arroba and a point.
+    function handleButtonInput() {
+        const inputValue = value;
         if (!inputValue.includes('@') && !inputValue.includes('.')){
             setValue('Invalid mail')
-            debugger
         } else {
             setValue('Mail correct')
-            debugger
         }
     };
 
-    function handleChangeInput(event){
+    // The setter of the useState of react.
+    function handleChangeInput(event) {
         const inputValue = event.target.value;
         setValue(inputValue);
-    }
+    };
+
     return(
         <form>
             <label htmlFor='inputId' className="label-title">Email Adress</label>
