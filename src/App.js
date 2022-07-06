d69953adc88f4c0d9e57a314a727a6bc150ab04c
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-05 20:48:35 -0500

    importing and fixing the modal portal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Header } from './components/App/Header';
 import { Form } from './components/App/Form';
 import './scss/globals.scss';
 
 function App() {
+
   return (
     <React.Fragment>
     <main className="main-class">
       <Header/>
-      <Form/>
+      <Form></Form>
     </main>
     </React.Fragment>
   );
 }
+
 
 export default App;
--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Modal } from '../Modal/index';
 import './form.scss';
 
 function Form() {
 
     const [value, setValue] = React.useState('Put your mail here');
     
-    // Check if the mail is in the correct format with an arroba and a point.
+    const [showModal, setShowModal] = React.useState(false);
+    const portalNode = document.getElementById('portal');
+
+    // Setter of the onClick function.
     function handleButtonInput() {
+        checkTextValue();
+        setShowModal(!showModal);
+    };
+
+    // Check if the mail is in the correct format with an arroba and a point.
+    function checkTextValue(){
         const inputValue = value;
         if (!inputValue.includes('@') && !inputValue.includes('.')){
-            setValue('Invalid mail')
+            setValue('Invalid mail');
         } else {
-            setValue('Mail correct')
+            setValue('Mail correct');
         }
     };
 
@@ -21,7 +32,8 @@ function Form() {
         setValue(inputValue);
     };
 
-    return(
+    return (
+        <>
         <form>
             <label htmlFor='inputId' className="label-title">Email Adress</label>
             <div className='form--input-button'>
@@ -29,6 +41,8 @@ function Form() {
             <button type='button' onClick={handleButtonInput}></button>
             </div>
         </form>
+        {showModal && ReactDOM.createPortal(<div><p>Hola que hace</p></div>, portalNode)}
+        </>
     );
 };
 
