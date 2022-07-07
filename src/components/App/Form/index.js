ece9760357d5b49a0555a2e977d77fe247643fe1
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-06 21:43:35 -0500

    SVG Arrow rigth FIXEEED!

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -37,7 +37,11 @@ function Form() {
             <label htmlFor='inputId' className="label-title">Email Adress</label>
             <div className='form--input-button'>
             <input onChange={handleChangeInput} type='text' id='inputId' value={value} placeholder={'Put your mail here'}className='input-class'name='inputId'></input>
-            <button type='button' onClick={handleButtonInput}></button>
+            <button type='button' onClick={handleButtonInput}>
+                <svg xmlns="http://www.w3.org/2000/svg" className='rigth-arrow-svg' viewBox="0 0 24 24">
+                    <path d="M11.293 5.707L16.586 11H5a1 1 0 000 2h11.586l-5.293 5.293a.999.999 0 101.414 1.414l7-7a1.006 1.006 0 000-1.414l-7-7a.999.999 0 10-1.414 1.414z"/>
+                </svg>
+            </button>
             </div>
         </form>
         {showModal && ReactDOM.createPortal(<ModalSucess/>, portalNode)}
