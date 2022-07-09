729cee34af789135d799b5c5807eb32c178627a7
Diego Fernando Ortiz Gamboa <diegofergamboa@hotmail.es>
2022-07-08 23:29:05 -0500

    tring to set the showModal with the true or false by the value options

--- src/components/App/Form/index.js
+++ src/components/App/Form/index.js
@@ -16,21 +16,13 @@ function Form() {
         const inputValue = event.target.value;
         setValue(inputValue);
     };
-
-    // Setter of the onClick function.
-    //! Use ref into the handleButtonInput.
-    function handleButtonInput() {
-    // Check if the mail is in the correct format setted.
+    
+    const handleChangeButton = () => {
         const inputValue = value;
-        if (!inputValue.includes('@') && !inputValue.includes('.')){
-            setValue('Invalid mail');
-            setShowModal(false);
-        } else {
-            setValue('Mail correct');
-            setShowModal(true);
-        }
-    };
-
+        (inputValue.includes('@')) ? setShowModal(true) : setShowModal(false);
+        console.log(showModal)
+    }
+    
 
     return (
         <>
@@ -38,15 +30,15 @@ function Form() {
             <label htmlFor='inputId' className="label-title">Email Adress</label>
             <div className='form--input-button'>
             <input onChange={handleChangeInput} type='text' id='inputId' value={value} placeholder={'Put your mail here'}className='input-class'name='inputId'></input>
-            <button type='button' onClick={handleButtonInput}>
+            <button type='button' onClick={handleChangeButton}>
                 <svg xmlns="http://www.w3.org/2000/svg" className='rigth-arrow-svg' viewBox="0 0 24 24">
                     <path d="M11.293 5.707L16.586 11H5a1 1 0 000 2h11.586l-5.293 5.293a.999.999 0 101.414 1.414l7-7a1.006 1.006 0 000-1.414l-7-7a.999.999 0 10-1.414 1.414z"/>
                 </svg>
             </button>
             </div>
         </form>
-        {!showModal && ReactDOM.createPortal(<ModalSucess/>, portalNode)}
-        {showModal && ReactDOM.createPortal(<ModalFailed/>, portalNode)}
+        {showModal && ReactDOM.createPortal(<ModalSucess/>, portalNode)}
+        {!showModal && ReactDOM.createPortal(<ModalFailed/>, portalNode)}
         </>
     );
 };
